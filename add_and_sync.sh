1616d7e334007a4ed4834fda3cd06b63c6de9dfd
quwip10 <alexh07@gmail.com>
2019-02-27 13:43:43 -0500

    removed names added variables

--- add_and_sync.sh
+++ add_and_sync.sh
@@ -3,14 +3,17 @@
 printf "Name: "
 read name
 
+printf "Domain: "
+read domain
+
 printf "IP: "
 read ip
 
 printf "Comment: "
 read comment
 
-printf "\n#$comment \n" >> /etc/pihole/testerbutt
-printf "$ip $name.heidenreich.hhn $name \n" >> /etc/pihole/testerbutt
+printf "\n#$comment \n" >> /etc/pihole/test_file
+printf "$ip $name.$domain $name \n" >> /etc/pihole/test_file
 
 pihole restartdns
 
