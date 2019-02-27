9d7f75e3c49b9eaeffd2190a34de1a948d96b93e
quwip10 <alexh07@gmail.com>
2019-02-27 15:44:55 -0500

    unnecessary

--- totalsync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#This script syncs the DNS lists, white lists, and black lists between both pihole devices.
-
-/usr/bin/rsync --progress --include \'black*\' --include \'white*\' --include \'lan.list\' --exclude \'*\' --dry-run /etc/pihole/ atlasalex@pihole2:/etc/pihole/
-
-#ssh -t atlasalex@pihole2 'pihole restartdns'
